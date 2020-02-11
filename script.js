326c0986d04366b658f15ba9da76af915bb29cb7
hylobates-lar <alison.v.quaglia@gmail.com>
2020-02-11 09:32:42 -0500

    flowers showing in the sidebar

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const sideBar = document.getElementById('sidebar');
 const sideBarButton = document.getElementById('open-sidebar');
 const mainContainer = document.getElementById('main');
 
-console.log(sideBar)
+
 
 /*----------------EVENT-LISTENERS------------*/
 
@@ -20,6 +20,34 @@ sideBarButton.onclick = () => {
 }
 
 /*----------------RENDERERS------------------*/
+const flowerList = document.querySelector("#flower-list")
+
+    function renderOneFlower(flower) {
+        const outerLi = document.createElement('li')
+        outerLi.className = "card"
+        outerLi.dataset.id = flower.id
+    
+        outerLi.innerHTML = `
+        <div class="image">
+            <img class="image" src="${flower.img_url}" alt="${flower.name}">
+        </div>
+        <div class="content">
+            <div class="name">${flower.name}</div>
+            <div class="meaning"><p>Meaning: ${flower.meaning}<p></div>
+            <div class="sound"><p>Sound: ${flower.sound}</p></div>
+        </div> `
+        flowerList.append(outerLi)
+    }
+
+    function renderAllFlowers(flowers) {
+        flowers.forEach(renderOneFlower)
+    }
+
+    fetch("http://localhost:3000/flowers")
+        .then(r => r.json())
+        .then(data => {
+            renderAllFlowers(data)
+        })
 
 
 });
