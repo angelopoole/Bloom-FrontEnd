a9231e91fdf373636bb654e81cb4e21f13a1522c
hylobates-lar <alison.v.quaglia@gmail.com>
2020-02-12 11:53:06 -0500

    saving some flower main changes

--- script.js
+++ script.js
@@ -10,6 +10,15 @@ const mainContainer = document.getElementById('main');
 
 /*----------------EVENT-LISTENERS------------*/
 
+// addButton.addEventListener("click", () => {
+//     console.log('clicked me')
+// })
+
+// closeButton.addEventListener("click", () => {
+//     console.log ('clicked me too')
+//     // let flowerMain = document.querySelector("#flower-main")
+//     // flowerMain.style.display = "none"
+// })
 
 /*----------------EVENT-HANDLERS-------------*/
 let sidebarOpen = false
@@ -45,34 +54,41 @@ const flowerList = document.querySelector("#flower-list")
 
         flowerSpan.addEventListener("click", () => {
             let flowerMain = document.querySelector("#flower-main")
+            flowerMain.style.display = "flex"
+
             flowerMain.innerHTML = `
-            <div class="main-image" style="background-image: url(${flower.img_url})"> </div>
-                <div class="content">
-                    <div class="name"><h2>${flower.name}</h2></div>
-                    <div class="meaning"><p>Meaning: ${flower.meaning}<p></div>
-                    <div class="sound"><p>Sound: ${flower.sound}</p></div>
-                    <button id="add-to-bouquet">Add to Bouquet</button>
-                    <button id="close">Close</button>
-                </div> `
+                <div class="main-image" style="background-image: url(${flower.img_url})"> </div>
+                    <div class="content">
+                        <div class="name"><h2>${flower.name}</h2></div>
+                        <div class="meaning"><p>Meaning: ${flower.meaning}<p></div>
+                        <div class="sound"><p>Sound: ${flower.sound}</p></div>
+                        <button id="add-to-bouquet">Add to Bouquet</button>
+                        <button id="close">Close</button>
+                    </div> `
 
-                const addButton = document.querySelector("#add-to-bouquet")
-                const closeButton = document.querySelector("#close")
-        
-                addButton.addEventListener("click", () => {
-                    console.log('clicked me')
-                })
-        
-                closeButton.addEventListener("click", () => {
-                    console.log('clicked me too')
-                })
-        })
+            const addButton = document.querySelector("#add-to-bouquet")
+            const closeButton = document.querySelector("#close")
+
+            addButton.addEventListener("click", () => {
+                console.log('clicked me')
+            })
+            console.log("click!!!!")
+            closeButton.addEventListener("click", () => {
+                console.log ('clicked me too')
+                // let flowerMain = document.querySelector("#flower-main")
+                flowerMain.style.display = "none"
+            })
+            console.log("click!!!! but after close button?")
+
+            })
     }
-
- 
-
+        
+        
     function renderAllFlowers(flowers) {
         flowers.forEach(renderOneFlower)
     }
+    
+
 
     fetch("http://localhost:3000/flowers")
         .then(r => r.json())
